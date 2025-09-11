4e0283edb48731fba6cea09e18728aeee75b1eb6
waquaralipatel <162323075+waquaralipatel@users.noreply.github.com>
2025-09-11 18:14:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import MainComponent from './components/MainComponent'
 import Footer from './components/Footer'
+import './App.css'
 
 const App = () => {
   return (
